056366a2c0de61cda1c0ee9f603d278cf89ec32c
Thomas Payne <tommypayne093@gmail.com>
2022-12-25 15:20:21 -0500

    Added files that expose the API for the library

---
+++ Engine/include/Mule/Mule.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+// ECS
+#include "ECS\Components.h"
+#include "ECS\Scene.h"
+#include "ECS\Entity.h"
+
+// Util
+#include "Ref.h"
+#include "Internal/ThreadPool.h"
+#include "Util/Log.h"
