a2af77e441d19fcb8e5af40f5c3bccbd3f393245
Thomas Payne <tommypayne093@gmail.com>
2024-06-25 20:07:57 -0400

    comfigured diligent engine build to use user defined imgui branch

--- Build-Windows.bat
+++ Build-Windows.bat
@@ -5,8 +5,13 @@ set cwd=%cd%
 
 if not exist "%cd%/SubModules/DiligentEngine/install" (
 	cd %cd%/SubModules/DiligentEngine
-	echo "Building Diligent Engine..."
-	cmake -S . -B ./build/Win64 -G "Visual Studio 17 2022" -A x64
+	echo Building Diligent Engine...
+	set "imgui_path=%cwd:\=/%/SubModules/imgui"
+	echo %imgui_path%
+	cmake -S . -B ./build/Win64 -G "Visual Studio 17 2022" -A x64 -DDILIGENT_DEAR_IMGUI_PATH=%imgui_path%
+	if not %errorlevel% == 0 (
+		goto :EOF
+	)
 	cd build/Win64
 	cmake --build . --config Release --target install
 	cmake --build . --target install
@@ -15,3 +20,5 @@ if not exist "%cd%/SubModules/DiligentEngine/install" (
 )
 
 premake5 vs2022
+
+:EOF
